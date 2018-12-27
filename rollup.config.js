73895f952b3800a1cf4d79fb1334f302ea5b209f
318 <seonhyung.jo@inswave.com>
2018-12-27 15:41:31 +0900

    init

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  input: 'src/index.js',
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    commonjs({
+      include: [
+        'node_modules/**',
+      ],
+    }),
+    babel({
+      exclude: 'node_modules/**', // only transpile our source code
+    }),
+    uglify({
+      compress: {
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ],
+  output: {
+    file: 'lib/index.js',
+    format: 'cjs',
+  },
+  sourcemap: false,
+};
