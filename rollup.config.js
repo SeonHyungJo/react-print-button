37c4b982eca80c38ddc5633c57f8a7da69a8e745
SeonHyungJo <seonhyung.jo@gmail.com>
2019-02-09 10:14:07 +0900

    :paw_prints: SeonHyungJo / Setting rollup cofig

--- rollup.config.js
+++ rollup.config.js
@@ -2,47 +2,44 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import uglify from 'rollup-plugin-uglify'
-import string from 'rollup-plugin-string'
+// import uglify from 'rollup-plugin-uglify'
+// import string from 'rollup-plugin-string'
 
 const dist = 'dist'
 const bundle = 'bundle'
 
 export default {
   input: 'src/PrintButton.js',
-  external: ['react'],
+  external: ['react', 'react-proptypes'],
   output: [
     {
       file: `${dist}/${bundle}.cjs.js`,
       format: 'cjs'
-    },
-    {
-      file: `${dist}/${bundle}.es.js`,
-      format: 'es'
-    },
-    {
-      name: 'ReactCssSpinners',
-      file: `${dist}/${bundle}.umd.js`,
-      globals: {
-        react: 'React'
-      },
-      format: 'umd'
     }
+    // {
+    //   file: `${dist}/${bundle}.es.js`,
+    //   format: 'es'
+    // },
+    // {
+    //   name: 'ReactCssSpinners',
+    //   file: `${dist}/${bundle}.umd.js`,
+    //   globals: {
+    //     react: 'React'
+    //   },
+    //   format: 'umd'
+    // }
   ],
   plugins: [
     resolve(),
-    // commonjs({
-    //   include: 'node_modules/**',
-    //   namedExports: {
-    //     '/react-dom/index.js': ['findDOMNode']
-    //   }
-    // }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
     babel({
       exclude: 'node_modules/**'
     }),
-    string({
-      include: '**/*.css'
-    }),
+    // string({
+    //   include: '**/*.css'
+    // }),
     // production & terser()
     terser()
     // uglify({
